2770f8a13fe52849260ea1176a507a5e06419beb
British Werewolf <britishwerewolfdev@gmail.com>
2024-06-25 21:00:37 +0100

    Create basic random picker. Remove unnecessary components.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,17 @@
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+    <h1>Random picker</h1>
+    <button @click="pickRandomItem">Pick an item!</button>
+    {{ chosenItem }}
 </template>
 
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue';
+import { ref } from 'vue';
+
+let chosenItem = ref<number>(0);
+const items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function pickRandomItem() {
+    const randomIndex = Math.floor(Math.random() * items.length);
+    chosenItem.value = items[randomIndex];
+}
 </script>
