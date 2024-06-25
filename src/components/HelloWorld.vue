3188e3695d80c676bed4b4b5671de586e8e273b7
British Werewolf <britishwerewolfdev@gmail.com>
2024-06-25 17:00:30 +0100

    Restructure components with template first, followed by script then style.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,3 @@
-<script setup lang="ts">
-defineProps<{
-  msg: string
-}>();
-</script>
-
 <template>
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
@@ -14,6 +8,12 @@ defineProps<{
     </h3>
   </div>
 </template>
+
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>();
+</script>
 
 <style scoped>
 h1 {
--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,12 +1,3 @@
-<script setup lang="ts">
-import WelcomeItem from './WelcomeItem.vue';
-import DocumentationIcon from './icons/IconDocumentation.vue';
-import ToolingIcon from './icons/IconTooling.vue';
-import EcosystemIcon from './icons/IconEcosystem.vue';
-import CommunityIcon from './icons/IconCommunity.vue';
-import SupportIcon from './icons/IconSupport.vue';
-</script>
-
 <template>
   <WelcomeItem>
     <template #icon>
@@ -86,3 +77,12 @@ import SupportIcon from './icons/IconSupport.vue';
     <a href="https://vuejs.org/sponsor/" target="_blank" rel="noopener">becoming a sponsor</a>.
   </WelcomeItem>
 </template>
+
+<script setup lang="ts">
+import WelcomeItem from './WelcomeItem.vue';
+import DocumentationIcon from './icons/IconDocumentation.vue';
+import ToolingIcon from './icons/IconTooling.vue';
+import EcosystemIcon from './icons/IconEcosystem.vue';
+import CommunityIcon from './icons/IconCommunity.vue';
+import SupportIcon from './icons/IconSupport.vue';
+</script>
