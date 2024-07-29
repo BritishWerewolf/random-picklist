bed6caecd107df1f629d8cab674089142ee86a51
British Werewolf <britishwerewolfdev@gmail.com>
2024-07-30 00:27:27 +0100

    Update navigation to instead use the Sheet component. Fix bug that added horizontal scrollbar on some browsers.

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,46 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router';
 import { useListsStore } from '@/stores/lists';
+import { Button } from '@/components/ui/button';
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '@/components/ui/sheet';
 
 const listsStore = useListsStore();
 </script>
 
 <template>
-  <nav>
-    <RouterLink :to="{ name: 'home' }">Home</RouterLink>
-    <RouterLink :to="{ name: 'create' }">Create list</RouterLink>
-    <RouterLink
-      v-for="list in listsStore.lists"
-      :key="`/list/${list.name}`"
-      :to="{ name: 'list', params: { name: list.name } }"
-    >
-      {{ list.name }}
-    </RouterLink>
+  <nav class="mb-4">
+    <Sheet>
+      <SheetTrigger>
+        <Button variant="outline">
+          Open Menu
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="left">
+        <SheetHeader>
+          <SheetTitle>Random Picklist</SheetTitle>
+        </SheetHeader>
+
+        <div class="flex flex-col gap-2 mt-4">
+          <RouterLink :to="{ name: 'home' }">Home</RouterLink>
+          <RouterLink :to="{ name: 'create' }">Create list</RouterLink>
+
+          <RouterLink v-for="list in listsStore.lists" :key="`/list/${list.name}`" :to="{ name: 'list', params: { name: list.name } }">
+            {{ list.name }}
+          </RouterLink>
+        </div>
+      </SheetContent>
+    </Sheet>
+
+    <a id="logo" href="/" class="ml-4 text-xl">Random Picklist</a>
   </nav>
 
   <div>
     <RouterView :key="$route.fullPath" />
   </div>
 </template>
-
-<style>
-nav {
-    position: fixed;
-    top: 0;
-    left: 0;
-    right: 0;
-    background-color: aquamarine;
-    color: black;
-    height: 50px;
-}
-
-#app {
-    margin-top: 50px;
-}
-</style>
